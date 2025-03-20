dc7056e7329fc9befa57fab3d9d5de0eaf894f81
dd-mm17 <diego.meraldi@epfl.ch>
2025-03-20 16:03:11 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cdkpackage",
+    version="0.1.0",
+    author="Diego",
+    author_email="diego.meraldi@epfl.ch",
+    description="A small utility to fetch and display SMILES structures as SVG using an external API.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/dd-mm17/CDK-Package",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "IPython",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
